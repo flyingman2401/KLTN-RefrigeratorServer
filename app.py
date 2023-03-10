2aba309bac74244b70e1a5c10497ebec601af6f9
flyingman2401 <84955172+flyingman2401@users.noreply.github.com>
2023-03-10 18:52:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from flask_jsonpify import jsonify
+
 app = Flask(__name__)
+
 @app.route('/')
 def hello():
-    return 'Hello, Trung!'
+    return 'Refrigerature\'s Server say hello to USER nhá!'
+
+@app.route('/SensorsData', methods = ['GET', 'POST'])
+def receiveData():
+    if request.method == 'POST':
+        return 'POST SUCCESS!'
+    elif request.method == 'GET':
+        return 'GET SUCCESS!'
+
+if __name__ == '__main__':
+   app.run(debug = True)
