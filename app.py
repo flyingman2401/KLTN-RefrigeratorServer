52428c3b24955a99dd274f9390c923a90860651c
flyingman2401 <flyingman24012001@gmail.com>
2023-03-10 22:47:51 +0700

    Modify app.py

--- app.py
+++ app.py
@@ -3,22 +3,27 @@ from flask_jsonpify import jsonify
 
 app = Flask(__name__)
 
+lastData = {
+    "temp": "0",
+    "humid":"0"
+}
 
 @app.route('/')
 def hello():
-    return 'Refrigerature\'s Server say hello to USER nhá!'
+    return "Hello tủ lạnh siêu thông minh!"
 
 @app.route('/SensorsData', methods = ['GET', 'POST'])
 def receiveData():
     if request.method == 'POST':
         data = request.get_json()
+        lastData['temp'] = data['temp']
         # temp = data['temp']
         # humid = data['humid']
         return data
         # return 'POST SUCCESS!'
     elif request.method == 'GET':
         # return jsonify(temp, humid)
-        return 'GET SUCCESS!'
+        return lastData
 
 if __name__ == '__main__':
    app.run(debug = True)
