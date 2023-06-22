85cfcb91ba70d83e29509b41becb3ff6eec23017
flyingman2401 <flyingman24012001@gmail.com>
2023-06-22 12:31:03 +0700

    Modify app.

--- templates/survey_app/flutter_service_worker.js
+++ templates/survey_app/flutter_service_worker.js
@@ -87,9 +87,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e95887b1216683b5590b3e60ce0f0a58",
-"/": "e95887b1216683b5590b3e60ce0f0a58",
-"main.dart.js": "9e1eb173336c26adedc7a901ea22396b",
+"index.html": "69ce8ad00d9833a73621e5e69e13d5ac",
+"/": "69ce8ad00d9833a73621e5e69e13d5ac",
+"main.dart.js": "71bcf50cf08e8236585c131214231152",
 "manifest.json": "4b6b5e001327ed513d8e458a14958c13",
 "version.json": "70601ec6b406c548fdeed13677fbb0ff"
 };
